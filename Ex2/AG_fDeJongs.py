a5b241bd8a478c722a7b8acd90af88efd1daf2af
Everton Alex <evertonalexweb@gmail.com>
2019-04-14 14:48:48 -0300

    ex2 inicio

---
+++ Ex2/AG_fDeJongs.py
@@ -0,0 +1,34 @@
+# Este AG resolve a funciton de Jong's
+# f1(x)sum(x(i)^s)
+# i = 1:n
+# -5.12<=(i)<=5.12
+# Function a ser maximizada -> f(x)=x^2
+
+
+class Individuo():
+    def __init__(self, cromossomo):
+        self.cromossomo = cromossomo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+POPULACAO = []
+POPULACAO[0] = Individuo("001100")
+POPULACAO[1] = "010101"
+POPULACAO[2] = "111000"
+POPULACAO[3] = "000111"
+POPULACAO[4] = "101011"
+POPULACAO[5] = "101000"
